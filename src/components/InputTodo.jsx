64d53fdb3e37401dd6cc6b61d9f57d5f8d4b6896
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-03-28 23:25:43 +0900

    カイゼン(CSS-in-JS) 一例

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+  backgroundColor: "#c1ffff"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder="Todoを入力" value={todoText} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
